5f9410fbbc3abbc57f640cf9282b6bf53d546658
André <43089870+andrebtw@users.noreply.github.com>
2022-07-22 15:35:28 +0200

    Add files via upload

---
+++ c_08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 01:47:28 by anrodri2          #+#    #+#             */
+/*   Updated: 2022/07/22 03:49:12 by anrodri2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
